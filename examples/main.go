02c99fb7406d46c550aedb21c40eebdc8eeeeabc
tkovs <tkovs@outlook.com>
2018-08-17 09:15:08 -0300

    Set the config to Sentry on Courier

--- examples/main.go
+++ examples/main.go
@@ -6,13 +6,18 @@ import (
 	"fmt"
 
 	courier ".."
+	raven "github.com/getsentry/raven-go"
 )
+
+func init() {
+	raven.SetDSN("https://db0b31ef93d043ceaac9aa6c96eb9be7:4f945b825be0415498a85def9e55a125@sentry.io/1263532")
+}
 
 func main() {
 	m := courier.Monitor{}
 	err := m.Start()
 
 	if err != nil {
-		fmt.Println("Erro:", err.Error())
+		fmt.Errorf(err.Error())
 	}
 }
